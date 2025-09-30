e3d22b7cb63b787aec963f2c313d4a850a6de170
joana de souza <joanasouzasilva.2008@gmail.com>
2025-09-30 08:42:01 -0300

    função cadastrar livros

--- main.py
+++ main.py
@@ -13,9 +13,27 @@ CREATE TABLE IF NOT EXISTS livros (
     titulo TEXT, 
     autor TEXT NOT NULL,
     ano INTEGER,
-    disponivel CHAR(3)        
-                           
+    disponivel CHAR(3)  CHECK(disponivel IN ('sim', 'nao'))          
 )
 
 """)
+
 print("Tabela criada com sucesso")
+
+#inserir um livro o banco de dados 
+def cadastrar_livro(nomelivro, autor, ano):
+    cursor.execute("""
+    INSERT INTO livros (titulo, autor, ano, disponivel) 
+    VALUES (?, ?, ?, 'sim')
+    """, (nomelivro, autor, ano)
+)
+    
+nomelivro = input("digite o nome do livro que deseja inserir: ")
+autor = input("digite o nome do autor: ")
+ano = int(input("digite o ano em que o livro foi postado: "))
+
+cadastrar_livro(nomelivro, autor, ano)
+
+conexao.commit()
+
+print("livro cadastrado com sucesso!")
